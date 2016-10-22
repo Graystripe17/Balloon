53fc159b845bf62ecb172c0b5b1987c3799b3912
Script Kitty <hakuna.matata.kitty@gmail.com>
2016-10-22 13:07:03 -0500

    Year spitout works

--- birthday.js
+++ birthday.js
@@ -1,16 +1,12 @@
 "use strict";
 
 
-
-
 document.addEventListener('DOMContentLoaded', function() {
-    console.log("DOM CONTENT LOADED");
     document.querySelector('button').addEventListener('click', main)
 })
 
 function main(event){
     event.preventDefault();
-    var source = document.getElementById('birthday-input').value;
-    document.getElementById('results').innerHTML = source;
-    console.log("MAIN");
+    var source = new Date(document.getElementById('birthday-input').value);
+    document.getElementById('results').innerHTML = source.getFullYear();
 }
