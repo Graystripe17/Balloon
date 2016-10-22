d42dfe3d3ac2b48917bcd363d48f2f365cdff4a9
Script Kitty <hakuna.matata.kitty@gmail.com>
2016-10-22 13:25:40 -0500

    Revert to datetime-local but keep all the extra details of hours and minutes and seconds

--- birthday.js
+++ birthday.js
@@ -8,5 +8,21 @@ document.addEventListener('DOMContentLoaded', function() {
 function main(event){
     event.preventDefault();
     var source = new Date(document.getElementById('birthday-input').value);
-    document.getElementById('results').innerHTML = source.getFullYear();
+    var display = document.getElementById('results').innerHTML
+    display = source.getFullYear();
+    
+    startTimer(display, source);
+}
+
+function startTimer(display, input){
+    var now = new Date()
+    setInterval(function(){
+                // This function displays milliseconds remaining
+                display = input - now;
+                
+                console.log(now - input)
+                console.log(now, "NOW")
+                console.log(input, "input")
+                
+    }, 2000)
 }
